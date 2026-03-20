1cec8a3005138e143e364387f07a0d17dceb0492
MathAvila42 <e.matheus.avila@gmail.com>
2026-03-20 17:25:44 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -630,11 +630,11 @@ window.addEventListener('DOMContentLoaded', function() {
       btnEnviar.disabled = true;
 
       // ATENÇÃO: Substitua pelos seus IDs gerados no passo 1
-      const serviceID = 'service_48c9hab';
+      const serviceID = 'service_sn6e8xg';
       const templateID = 'template_u4i4ypt';
 
       // Dispara o e-mail
-      emailjs.sendForm('service_48c9hab', 'template_u4i4ypt', this)
+      emailjs.sendForm('service_sn6e8xg', 'template_u4i4ypt', this)
         .then(function() {
           alert('Mensagem enviada com sucesso! Entraremos em contato em breve.');
           formContato.reset(); // Limpa os campos do formulário
